268a93b5e1a02b67df136528bb30eac3aad8aa80
Pierre Cailleaud <pierre.cailleaud@gmail.com>
2020-09-23 16:02:13 +0200

    9-OperationPATCH

--- src/main/java/com/formation/restaurant/rest/RestaurantController.java
+++ src/main/java/com/formation/restaurant/rest/RestaurantController.java
@@ -1,11 +1,18 @@
 package com.formation.restaurant.rest;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.restaurant.exceptions.ResourceNotFoundException;
@@ -31,5 +38,29 @@ public class RestaurantController {
 			throw new ResourceNotFoundException();
 		}
 		return reponse;
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public String create(@RequestBody Restaurant restaurant) {
+		return restoService.create(restaurant);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void update(@PathVariable("id") String identifiant, @RequestBody Restaurant restaurant) {
+		if(restoService.findById(identifiant) == null) {
+			throw new ResourceNotFoundException();
+		}
+		restoService.update(identifiant, restaurant);
+	}
+	
+	@PatchMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void partialUpdate(@PathVariable("id") String identifiant, @RequestBody Map<String, Object> updates) {
+		if(restoService.findById(identifiant) == null) {
+			throw new ResourceNotFoundException();
+		}
+		restoService.partialUpdate(identifiant, updates);
 	}
 }
